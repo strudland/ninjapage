88feea0a511650ecfe2110a02262be6e784f4f69
Andrej Strudl <lindic.andrej@gmail.com>
2017-09-28 11:15:08 +0200

    base.py repair

--- handlers/base.py
+++ handlers/base.py
@@ -1,5 +1,8 @@
 import os
+import webapp2
 import jinja2
+
+
 
 template_dir = os.path.join(os.path.dirname(__file__), "../templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
