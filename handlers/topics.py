5b698453b6a97f0ba27f68099d408e8b450fb1fa
Andrej Strudl <lindic.andrej@gmail.com>
2017-10-08 22:59:39 +0200

    added topic page and coments

--- handlers/topics.py
+++ handlers/topics.py
@@ -1,27 +1,68 @@
+import uuid
+import cgi
+from google.appengine.api import memcache
 from handlers.base import BaseHandler
 from google.appengine.api import users
 from models.topic import Topic
+from models.comment import Comment
+
+def make_csrf_token():
+     csrf_token = str(uuid.uuid4())
+     memcache.add(csrf_token, True, time=600)
+     return csrf_token
 
 class TopicAdd(BaseHandler):
     def get(self):
-        return self.render_template("topic_add.html")
+        params = {
+            "csrf_token" : make_csrf_token()
+        }
+
+        return self.render_template("topic_add.html",params)
 
     def post(self):
+        csrf_token = self.request.get('csrf-token')
+        if not memcache.get(csrf_token):
+            return self.write("CSRF NAPAD")
         user = users.get_current_user()
 
         if not user:
             return self.write("Please login before you're allowed to post a topic.")
 
-        title = self.request.get("title")
-        text = self.request.get("text")
+        title = cgi.escape(self.request.get("title"))
+        text = cgi.escape(self.request.get("text"))
 
         new_topic = Topic(title=title, content=text, user_email=user.email())
         new_topic.put()  # put() saves the object in Datastore
 
         return self.redirect('/')
 
-class TopicHandler(BaseHandler):
-    def get(self, topic_id):
-        topic = Topic.get_by_id(int(topic_id))
-        params = {"topic": topic}
-        return self.render_template("topic.html", params=params)
+
+
+
+class TopicDetailsHandler(BaseHandler):
+     def get(self, topic_id):
+         topic = Topic.get_by_id(int(topic_id))
+         comments = Comment.query(Comment.topic_id == int(topic_id)).fetch()
+
+         params = {
+             "csrf_token": make_csrf_token(),
+             "topic": topic,
+             "comments": comments,
+         }
+         return self.render_template("topic_details.html",
+                                    params)
+
+     def post(self, topic_id):
+         csrf_token = self.request.get('csrf-token')
+         if not memcache.get(csrf_token):
+             return self.write("CSRF NAPAD")
+
+         text = cgi.escape(self.request.get('text'))
+
+         email = users.get_current_user().email()
+
+         topic = Topic.get_by_id(int(topic_id))
+
+         comment = Comment(content=text, user_email=email, topic_id=int(topic_id), topic_title=topic.title)
+         comment.put()
+         return self.redirect('/')
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import webapp2
 from handlers.main import MainHandler
 from handlers.cookie import CookieHandler
-from handlers.topics import TopicAdd,TopicHandler
+from handlers.topics import TopicAdd,TopicDetailsHandler
 
 
 
@@ -12,4 +12,5 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name="main-page"),
     webapp2.Route('/set-cookie', CookieHandler, name="main-page"),
     webapp2.Route('/topic/add', TopicAdd, name="topic-add"),
+    webapp2.Route('/topic-details/<topic_id:\d+>', TopicDetailsHandler, name="topic-details"),
 ], debug=True)
